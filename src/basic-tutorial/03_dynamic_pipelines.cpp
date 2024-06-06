3e68522903980aea660c154123b7e51ae47fb915
Kohei Noda <k.noda@saver.jp>
2024-06-06 18:32:25 +0900

    add basic tutor 03 but pad_added_handler is missing.

---
+++ src/basic-tutorial/03_dynamic_pipelines.cpp
@@ -0,0 +1,111 @@
+#include <gst/gst.h>
+#ifdef __APPLE__
+#include <TargetConditionals.h>
+#endif
+
+typedef struct _CustomData
+{
+    GstElement *pipeline;
+    GstElement *source;
+    GstElement *convert;
+    GstElement *resample;
+    GstElement *sink;
+} CustomData;
+
+static void pad_added_handler(GstElement *src, GstPad *pad, CustomData *data);
+
+int tutorial_main(int argc, char *argv[])
+{
+    gst_init(&argc, &argv);
+
+    CustomData data;
+    data.source = gst_element_factory_make("uridecodebin", "source");
+    data.convert = gst_element_factory_make("audioconver", "convert");
+    data.resample = gst_element_factory_make("audioresample", "resample");
+    data.sink = gst_element_factory_make("audoaudiosink", "sink");
+
+    data.pipeline = gst_pipeline_new("test-pipeline");
+
+    if (!data.pipeline || !data.source || !data.convert || !data.resample || !data.sink)
+    {
+        g_printerr("Not alll elements could be created.\n");
+        return -1;
+    }
+
+    gst_bin_add_many(GST_BIN(data.pipeline), data.source, data.convert, data.resample, data.sink, NULL);
+    if (!gst_element_link_many(data.source, data.convert, data.resample, data.sink, NULL) == TRUE)
+    {
+        g_printerr("Elements could not be linked.\n");
+        gst_object_unref(data.pipeline);
+        return -1;
+    }
+
+    g_object_set(data.source, "uri", "https://gstreamer.freedesktop.org/data/media/sintel_trailer-480p.webm", NULL);
+
+    g_signal_connect(data.source, "pad-added", G_CALLBACK(pad_added_handler), &data);
+
+    if (gst_element_set_state(data.pipeline, GST_STATE_PLAYING) == GST_STATE_CHANGE_FAILURE)
+    {
+        g_printerr("Unable to set the pipeline to the playing state.\n");
+        gst_object_unref(data.pipeline);
+        return -1;
+    }
+
+    auto bus = gst_element_get_bus(data.pipeline);
+    gboolean terminate = FALSE;
+    do
+    {
+        auto msg =
+            gst_bus_timed_pop_filtered(bus, GST_CLOCK_TIME_NONE,
+                                       GST_MESSAGE_STATE_CHANGED | GST_MESSAGE_ERROR | GST_MESSAGE_EOS);
+        if (msg != NULL)
+        {
+            switch (GST_MESSAGE_TYPE(msg))
+            {
+            case GST_MESSAGE_ERROR:
+                GError *err;
+                gchar *debug_info;
+
+                gst_message_parse_error(msg, &err, &debug_info);
+                g_printerr("Error received from element %s: %s\n", GST_OBJECT_NAME(msg->src), err->message);
+                g_printerr("Debugging information: %s\n", debug_info ? debug_info : "none");
+                g_clear_error(&err);
+                g_free(debug_info);
+                terminate = TRUE;
+                break;
+            case GST_MESSAGE_EOS:
+                g_print("End-Of-Stream reached.\n");
+                terminate = TRUE;
+                break;
+            case GST_MESSAGE_STATE_CHANGED:
+                if (GST_MESSAGE_SRC(msg) == GST_OBJECT(data.pipeline))
+                {
+                    GstState old_state, new_state, pending_state;
+                    gst_message_parse_state_changed(msg, &old_state, &new_state, &pending_state);
+                    g_print("Pipeline state change form %s to %s:\n",
+                            gst_element_state_get_name(old_state), gst_element_state_get_name(new_state));
+                }
+                break;
+            default:
+                g_printerr("Unexpected message received.\n");
+                break;
+            }
+            gst_message_unref(msg);
+        }
+
+    } while (!terminate);
+
+    gst_object_unref(bus);
+    gst_element_set_state(data.pipeline, GST_STATE_NULL);
+    gst_object_unref(data.pipeline);
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+#if defined(__APPLE__) && TARGET_OS_MAC && !TARGET_OS_IPHONE
+    return gst_macos_main((GstMainFunc)tutorial_main, argc, argv, NULL);
+#else
+    return tutorial_main(argc, argv);
+#endif
+}
